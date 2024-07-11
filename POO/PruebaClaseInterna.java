b3231c97722e0126551551eff55bb09e60463442
lemac1nside <slemac@unicartagena.edu.co>
2024-07-11 12:24:00 -0500

    Clase interna local

--- POO/PruebaClaseInterna.java
+++ POO/PruebaClaseInterna.java
@@ -10,41 +10,32 @@ import javax.swing.Timer;
 
 public class PruebaClaseInterna {
     public static void main(String[] args) {
-        Reloj miReloj = new Reloj(3000, true);
-        miReloj.enMarcha();
+        Reloj miReloj = new Reloj();
+        miReloj.enMarcha(3000, true);
 
         JOptionPane.showMessageDialog(null, "Pulsa Ok para terminar");
         System.exit(0);
     }
 }
 
-class Reloj{
+class Reloj {
 
-    public Reloj(int intervalo, boolean sonido){
-        this.intervalo = intervalo;
-        this.sonido = sonido;
-    }
+    public void enMarcha(int intervalo, final boolean sonido) {
+        // Clase Interna - y ahora es clase interna local
+        class DameLaHora2 implements ActionListener {
+            public void actionPerformed(ActionEvent evento) {
+                Date ahora = new Date();
+                System.out.println("Te doy la hora cada 3 segs: " + ahora);
 
-    public void enMarcha(){
+                if (sonido) {
+                    Toolkit.getDefaultToolkit().beep();
+                }
+            }
+        }
         ActionListener oyente = new DameLaHora2();
 
         Timer miTemporizador = new Timer(intervalo, oyente);
 
         miTemporizador.start();
-    }
-
-    private int intervalo;
-    private boolean sonido;
-    
-    //Clase Interna
-    private class DameLaHora2 implements ActionListener{
-        public void actionPerformed(ActionEvent evento){
-            Date ahora = new Date();
-            System.out.println("Te doy la hora cada 3 segs: " + ahora);
-
-            if(sonido){
-                Toolkit.getDefaultToolkit().beep();
-            }
-        }
     }
 }
