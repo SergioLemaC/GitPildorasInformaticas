94ce4ec772730c5fa490f7757b04139457cb27e3
Sergio <sergio011304@gmail.com>
2024-07-11 18:08:09 -0500

    Empezando con las pantallitas

---
+++ graficos/CreandoMarcos.java
@@ -0,0 +1,23 @@
+package graficos;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Sergio
+ */
+public class CreandoMarcos {
+    public static void main(String[] args) {
+        MiMarco marco1 = new MiMarco();
+        
+        marco1.setVisible(true);
+        
+        marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+class MiMarco extends JFrame{
+    public MiMarco(){
+        setSize(500, 300);
+    }
+}
