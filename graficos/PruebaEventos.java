f38182ca44541f924173c972c6620c3cd6f254dc
lemac1nside <slemac@unicartagena.edu.co>
2024-07-15 14:27:51 -0500

    Eventos de ratón con clases internas

--- graficos/PruebaEventos.java
+++ graficos/PruebaEventos.java
@@ -16,54 +16,83 @@ public class PruebaEventos {
     }
 }
 
-class MarcoConBotones extends JFrame{
-    public MarcoConBotones(){
+class MarcoConBotones extends JFrame {
+    public MarcoConBotones() {
         setTitle("Botones y eventos");
-        setSize(1000,1000);
+        setSize(1000, 1000);
         LaminaBotones miLamina = new LaminaBotones();
         add(miLamina);
 
-        //Acá para que todos los objetos de ese tipo tengan las mismas propiedades
-        //miLamina.setForeground(Color.BLUE);
+        // Acá para que todos los objetos de ese tipo tengan las mismas propiedades
+        // miLamina.setForeground(Color.BLUE);
     }
 }
 
-class LaminaBotones extends JPanel implements ActionListener{
-    //Creamos el botón
+class LaminaBotones extends JPanel implements ActionListener {
+    // Creamos el botón
     JButton botonNegro = new JButton("Negro");
     JButton botonAmarillo = new JButton("Amarillo");
     JButton botonRojo = new JButton("Rojo");
-    
-    public LaminaBotones(){
+
+    public LaminaBotones() {
         add(botonNegro);
         add(botonAmarillo);
         add(botonRojo);
-        //El botón va a desencadenar un evento de tipo clic
-        //y el objeto que tiene que estar a la escucha es this, osea la propia clase
-        //Declaramos 3 objetos fuente
-        botonNegro.addActionListener(this); //Al hacer clic
+
+        /*
+         * ColorFondo amarillo = new ColorFondo(Color.YELLOW);
+         * ColorFondo negro = new ColorFondo(Color.BLACK);
+         * ColorFondo rojo = new ColorFondo(Color.RED);
+         * 
+         *  botonNegro.addActionListener(negro); // Al hacer clic
+            botonRojo.addActionListener(rojo);
+            botonAmarillo.addActionListener(amarillo);
+         */
+
+        // El botón va a desencadenar un evento de tipo clic
+        // y el objeto que tiene que estar a la escucha es this, osea la propia clase
+        // Declaramos 3 objetos fuente
+        botonNegro.addActionListener(this); // Al hacer clic
         botonRojo.addActionListener(this);
         botonAmarillo.addActionListener(this);
     }
 
-    //Obligatoriamente debemos construir la función actionPerformed() ya que la clase 
-    //implementa la interfaz ActionListener
-    public void actionPerformed(ActionEvent e){ //Después de que tenga lugar ese evento, se crearán objetos de tipo event y se almacenarán en la (e)
-        //getSource() permite capturar el origen del evento
+    // Obligatoriamente debemos construir la función actionPerformed() ya que la
+    // clase
+    // implementa la interfaz ActionListener
+    public void actionPerformed(ActionEvent e) { // Después de que tenga lugar ese evento, se crearán objetos de tipo
+                                                 // event y se almacenarán en la (e)
+        // getSource() permite capturar el origen del evento
         Object botonPulsado = e.getSource();
-        if(botonPulsado==botonNegro){
+        if (botonPulsado == botonNegro) {
             setBackground(Color.BLACK);
-        }else if(botonPulsado==botonAmarillo){
+        } else if (botonPulsado == botonAmarillo) {
             setBackground(Color.YELLOW);
-        }else{
+        } else {
             setBackground(Color.RED);
         }
-        
+
     }
 
-    /*Lo que ocurre es que cuando le damos clic al botón
+    /*
+     * Lo que ocurre es que cuando le damos clic al botón
      * Java construye un objeto de tipo evento y ese objeto
-     * automáticamente viaja al objeto de tipo ActionEvent 
+     * automáticamente viaja al objeto de tipo ActionEvent
      * de la función actionPerformed() y se ejecuta esa función
+     */
+
+    /*CLASE INTERNA - OTRA FORMA DE PROGRAMARLO
+     * private class ColorFondo implements ActionListener{
+     * 
+        * public colorFondo(Color c){
+        *      colorDeFondo = c;
+        * }
+     * 
+        * public void actionPerformed(ActionEvent e){
+        *      setBackground(colorDeFondo);
+        * }
+        * 
+        * private Color colorDeFondo;
+     * }
      */
 }
