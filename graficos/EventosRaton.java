d14aba4dea89575668c82201ef8b886f807ad13f
lemac1nside <slemac@unicartagena.edu.co>
2024-07-18 13:11:16 -0500

    Más eventos de ratón, en este caso cuando pasas el mouse o cuando arrastras con el clic

--- graficos/EventosRaton.java
+++ graficos/EventosRaton.java
@@ -1,8 +1,10 @@
 package graficos;
 
+import java.awt.event.InputEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 //import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JFrame;
 
@@ -19,14 +21,45 @@ class MarcoRaton extends JFrame{
         setBounds(700, 300, 600, 450);
 
         //EventosDeRaton eventoRaton = new EventosDeRaton();
-        addMouseListener(new EventosDeRaton());
+        addMouseMotionListener(new EventosDeRaton());
     }
 }
 
-class EventosDeRaton extends MouseAdapter{
+class EventosDeRaton extends MouseAdapter implements MouseMotionListener{
     public void mouseClicked(MouseEvent e){
         System.out.println("Haz hecho click");
+        System.out.println("Coordenada X: " + e.getX() + "\n" +
+        "Coordenada Y: " + e.getY());
+        System.out.println(e.getClickCount());
+
     }
+
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        //System.out.println("Acabas de presionar");
+        //System.out.println(e.getModifiersEx());
+        if(e.getModifiersEx() == InputEvent/*MouseEvent*/.BUTTON1_DOWN_MASK){
+            System.out.println("Haz pulsado el click izquierdo");
+        }else if(e.getModifiersEx() == MouseEvent.BUTTON2_DOWN_MASK){
+            System.out.println("Haz pulsado la rueda del ratón");
+        }else if(e.getModifiersEx() == MouseEvent.BUTTON3_DOWN_MASK){
+            System.out.println("Has pulsado el click derecho");
+        }
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Estás arrastrando");
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Estás moviendo el ratón");
+    }
+
+    
 }
 
 /*class EventosDeRaton implements MouseListener{
