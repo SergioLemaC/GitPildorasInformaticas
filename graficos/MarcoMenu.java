437d8477acb340d11015e333bb0971688eb489b6
lemac1nside <slemac@unicartagena.edu.co>
2024-07-26 11:23:07 -0500

    Creando menús con las clases JMenuBar, JMenu y JMenuItem

---
+++ graficos/MarcoMenu.java
@@ -0,0 +1,63 @@
+package graficos;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+public class MarcoMenu {
+    public static void main(String[] args) {
+        MenuFrame miMarco = new MenuFrame();
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+class MenuFrame extends JFrame{
+    public MenuFrame(){
+        setBounds(500, 300, 550, 400);
+
+        MenuLamina miLamina = new MenuLamina();
+        add(miLamina);
+        setVisible(true);
+    }
+}
+
+class MenuLamina extends JPanel{
+    public MenuLamina(){
+        JMenuBar miBarra = new JMenuBar();
+
+        JMenu archivo = new JMenu("Archivo");
+        JMenu edicion = new JMenu("Edición");
+        JMenu herramientas = new JMenu("Herramientas");
+        JMenu opciones = new JMenu("Opciones");
+
+        JMenuItem guardar = new JMenuItem("Guardar");
+        JMenuItem guardarComo = new JMenuItem("Guardar Como");
+        archivo.add(guardar);
+        archivo.add(guardarComo);
+
+        JMenuItem cortar = new JMenuItem("Cortar");
+        JMenuItem copiar = new JMenuItem("Copiar");
+        JMenuItem pegar = new JMenuItem("Pegar");
+        edicion.add(cortar);
+        edicion.add(copiar);
+        edicion.add(pegar);
+        edicion.addSeparator(); //Agregar una línea separadora
+
+        JMenuItem generales = new JMenuItem("Generales");
+        herramientas.add(generales);
+
+        JMenuItem opcion1 = new JMenuItem("Opción 1");
+        JMenuItem opcion2 = new JMenuItem("Opción 2");
+        opciones.add(opcion1);
+        opciones.add(opcion2);
+        edicion.add(opciones); //Agregarlo a otro item de opciones
+
+        miBarra.add(archivo);
+        miBarra.add(edicion);
+        miBarra.add(herramientas);
+
+        add(miBarra);
+    }
+}
