7ae6e0caf866a425c47739ffebeae515665ce2fb
lemac1nside <slemac@unicartagena.edu.co>
2024-07-26 14:20:01 -0500

    Practica guiada de un procesador de textos sencillo

---
+++ graficos/Procesador_PuraPractica.java
@@ -0,0 +1,145 @@
+package graficos;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.text.StyledEditorKit;
+
+public class Procesador_PuraPractica {
+    public static void main(String[] args) {
+        MenuProcesador miMarco = new MenuProcesador();
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+class MenuProcesador extends JFrame{
+    public MenuProcesador(){
+        setBounds(500, 300, 550, 400);
+
+        LaminaProcesador miLamina = new LaminaProcesador();
+        add(miLamina);
+        setVisible(true);
+    }
+}
+
+class LaminaProcesador extends JPanel{
+    public LaminaProcesador(){
+        setLayout(new BorderLayout());
+
+        JPanel laminaMenu = new JPanel();
+
+        JMenuBar miBarra = new JMenuBar();
+
+        fuente = new JMenu("Fuente");
+        estilo = new JMenu("Estilo");
+        tamaño = new JMenu("Tamaño");
+
+        configuraMenu("Arial", "fuente", "Arial", 9, 10);
+        configuraMenu("Courier", "fuente", "Courier", 9, 10);
+        configuraMenu("Verdana", "fuente", "Verdana", 9, 10);
+
+        configuraMenu("Negrita", "estilo", "", Font.BOLD, 1);
+        configuraMenu("Cursiva", "estilo", "", Font.ITALIC, 1);
+
+        configuraMenu("12", "tamaño", "", 9, 12);
+        configuraMenu("16", "tamaño", "", 9, 16);
+        configuraMenu("20", "tamaño", "", 9, 20);
+        configuraMenu("24", "tamaño", "", 9, 24);
+
+        miBarra.add(fuente);
+        miBarra.add(estilo);
+        miBarra.add(tamaño);
+
+        laminaMenu.add(miBarra);
+
+        add(laminaMenu, BorderLayout.NORTH);
+
+        miArea = new JTextPane();
+
+        add(miArea, BorderLayout.CENTER);
+    }
+
+    public void configuraMenu(String rotulo, String menu, String tipoLetra, int estilos, int tam){
+        JMenuItem elemMenu = new JMenuItem(rotulo);
+
+        if(menu.equals("fuente")){
+            fuente.add(elemMenu);
+
+            if(tipoLetra.equals("Arial")){
+                elemMenu.addActionListener(new StyledEditorKit.FontFamilyAction("cambiaLetra" , "Arial"));
+            }else if(tipoLetra.equals("Courier")){
+                elemMenu.addActionListener(new StyledEditorKit.FontFamilyAction("cambiaLetra" , "Courier"));
+            }else if(tipoLetra.equals("Verdana")){
+                elemMenu.addActionListener(new StyledEditorKit.FontFamilyAction("cambiaLetra" , "Verdana"));
+            }
+        }else if(menu.equals("estilo")){
+            estilo.add(elemMenu);
+
+            if(estilos == Font.BOLD){
+                elemMenu.addActionListener(new StyledEditorKit.BoldAction());
+            }else if(estilos == Font.ITALIC){
+                elemMenu.addActionListener(new StyledEditorKit.ItalicAction());
+            }
+        }else if(menu.equals("tamaño")){
+            tamaño.add(elemMenu);
+
+            elemMenu.addActionListener(new StyledEditorKit.FontSizeAction("cambiaTamaño", tam));
+        }
+
+        //elemMenu.addActionListener(new GestionaEventos(rotulo, tipoLetra, estilos, tam));
+    }
+
+    //PARA USAR LA CLASE -StyledEditorKit- NOS SOBRA TODA LA CLASE INTERNA
+
+    /*private class GestionaEventos implements ActionListener{
+
+        String tipoTexto, menu;
+        int estiloLetra, tamañoLetra;
+
+        GestionaEventos(String elemento, String texto2, int estilo2, int tamLetra){
+            tipoTexto = texto2;
+            estiloLetra = estilo2;
+            tamañoLetra = tamLetra;
+            menu = elemento;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // TODO Auto-generated method stub
+            
+            letras = miArea.getFont();
+
+            if(menu.equalsIgnoreCase("Arial") || menu.equalsIgnoreCase("Courier") || menu.equalsIgnoreCase("Verdana")){
+                estiloLetra = letras.getStyle();
+
+                tamañoLetra = letras.getSize();
+            }else if(menu.equalsIgnoreCase("Cursiva") || menu.equalsIgnoreCase("Negrita")){
+                if(letras.getStyle() == 1 || letras.getStyle() == 2){
+                    estiloLetra = 3;
+                }
+                
+                tipoTexto = letras.getFontName();
+
+                tamañoLetra = letras.getSize();
+            }else if(menu.equalsIgnoreCase("12") || menu.equalsIgnoreCase("16") || menu.equalsIgnoreCase("20") || menu.equalsIgnoreCase("24")){
+                estiloLetra = letras.getStyle();
+
+                tipoTexto = letras.getFontName();
+            }
+
+            miArea.setFont(new Font(tipoTexto, estiloLetra, tamañoLetra));
+
+            System.out.println("Tipo: " + tipoTexto + " Estilo: " + estiloLetra + " Tamaño: " + tamañoLetra);
+        }
+        
+    }*/
+
+    private JTextPane miArea;
+    private JMenu fuente, estilo, tamaño;
+}
