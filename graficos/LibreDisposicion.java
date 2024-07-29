35919ebcd546bd1fedbaa4f9f375a974f3da558d
lemac1nside <slemac@unicartagena.edu.co>
2024-07-29 14:00:07 -0500

    Siguiendo las disposiciones

--- graficos/LibreDisposicion.java
+++ graficos/LibreDisposicion.java
@@ -1,8 +1,14 @@
 package graficos;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.LayoutManager;
 
 public class LibreDisposicion {
     public static void main(String[] args) {
@@ -24,10 +30,71 @@ class MarcoLibre extends JFrame{
 
 class LaminaLibre extends JPanel{
     public LaminaLibre(){
-        setLayout(null);
+        setLayout(new EnColumnas());
 
-        JButton boton1 = new JButton("Botón 1");
-        boton1.setBounds(50, 50, 150, 50);
-        add(boton1);
+        JLabel nombre = new JLabel("Nombre: ");
+        JLabel apellido = new JLabel("Apellido: ");
+
+        JTextField cNombre = new JTextField();
+        JTextField cApellido = new JTextField();
+
+        add(nombre);
+        add(cNombre);
+        add(apellido);
+        add(cApellido);
     }
+}
+
+class EnColumnas implements LayoutManager{
+
+    @Override
+    public void addLayoutComponent(String name, Component comp) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void layoutContainer(Container parent) {
+        // TODO Auto-generated method stub
+        
+        int contador = 0;
+        int n = parent.getComponentCount();
+
+        for(int i = 0; i < n; i++){
+            contador++;
+
+            Component c = parent.getComponent(i);
+
+            c.setBounds(x, y, 100, 20);
+
+            x += 100;
+
+            if(contador % 2 == 0){
+                x = 20;
+                y += 40;
+            }
+        }
+    }
+
+    @Override
+    public Dimension minimumLayoutSize(Container parent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Dimension preferredLayoutSize(Container parent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void removeLayoutComponent(Component comp) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private int x = 20;
+    private int y = 20;
+    
 }
