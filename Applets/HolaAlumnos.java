24cca1ce9b0e64cc3e10b96ae89de609f4b431c5
lemac1nside <slemac@unicartagena.edu.co>
2024-08-11 13:08:10 -0500

    Applets con otra librería

--- Applets/HolaAlumnos.java
+++ Applets/HolaAlumnos.java
@@ -2,11 +2,12 @@ package Applets;
 
 import javax.swing.JApplet;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class HolaAlumnos extends JApplet{
     //No hay método main, sino init()
     public void init(){
-        JLabel rotulo = new JLabel("Hola alumno");
+        JLabel rotulo = new JLabel("Hola alumno", SwingConstants.CENTER);
         add(rotulo);
     }
 }
