e3dcdd627a1aeb0ec2370601775d4834d3ebfd56
lemac1nside <slemac@unicartagena.edu.co>
2024-10-15 11:57:28 -0500

    Todo configurado con los drivers para conectar a diferentes bases de datos (MYSQL, ORACLE, SQLSERVER, PORTGRESQL)

--- BBDD_JDBC/ConectaUsuarios.java
+++ BBDD_JDBC/ConectaUsuarios.java
@@ -1,4 +1,4 @@
-package BBDD_JDCB;
+package BBDD_JDBC;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,14 +9,25 @@ import java.sql.Statement;
 public class ConectaUsuarios {
     public static void main(String[] args) {
         try {
-            Connection miConexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/usuarios", "root", "Sergio011304*");
+            //MYSQL
+            //Connection miConexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/usuarios", "root", "Sergio011304*");
+
+            //ORACLE
+            //Primero desde la terminal hay que iniciar el servicio de Oracle "lsnrctl start"
+            //Connection miConexion = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "SYSTEM", "Sergio011304*");
+
+            //SQLSERVER
+            //Connection miConexion = DriverManager.getConnection("jdbc:sqlserver://DESKTOP-P70AUTP:1433;databaseName=elqsea;trustServerCertificate=true" ,"lemac1nside", "Sergio011304*");
+
+            //POSTGRESQL
+            Connection miConexion = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "Sergio011304*");
 
             Statement miStatement = miConexion.createStatement();
 
-            ResultSet miResultSet = miStatement.executeQuery("SELECT * FROM usuarios");
+            ResultSet miResultSet = miStatement.executeQuery("SELECT * FROM USUARIOS");
 
             while(miResultSet.next()){
-                System.out.println(miResultSet.getString("cedula") + " | " + miResultSet.getString("nombre") + " | " + miResultSet.getString("apellido"));
+                System.out.println(miResultSet.getString("CEDULA") + " | " + miResultSet.getString("NOMBRE") + " | " + miResultSet.getString("APELLIDO"));
             }
         } catch (SQLException e) {
             // TODO Auto-generated catch block
